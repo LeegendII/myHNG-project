54d99fcaf1354146aeec12ff8c40f2145b60ea3a
lone-DEV <waves19.teewhy@gmail.com>
2025-10-18 23:30:02 +0100

    Delete my hng project directory

--- my hng project/profile-card.js
+++
@@ -1,8 +0,0 @@
-function updateUTCTime() {
-    const now = new Date();
-    const utcTime = now.toUTCString();
-    document.querySelector('[data-testid="currentTimeUTC"]').textContent = utcTime;
-}
-updateUTCTime();
-// Update time every second
-setInterval(updateUTCTime, 1000);
